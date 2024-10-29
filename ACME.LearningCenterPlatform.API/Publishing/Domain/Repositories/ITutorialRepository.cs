2d8d8001c9506648831252cb627ffac7853e2840
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-28 20:25:33 -0500

    feat(tutorials): add repository contracts

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Repositories/ITutorialRepository.cs
@@ -0,0 +1,12 @@
+﻿using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories
+{
+    public interface ITutorialRepository : IBaseRepository<Tutorial>
+    {
+
+        Task<IEnumerable<Tutorial>> FindByCategoryIdAsync(int cateogoryId);
+
+    }
+}
