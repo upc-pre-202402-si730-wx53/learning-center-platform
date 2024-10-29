e393f737805cf83b712e0e9dc05ff94b2f3771a7
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-28 21:26:07 -0500

    feat(tutorials): add outbound service implementatios

---
+++ ACME.LearningCenterPlatform.API/Publishing/Infrastructure/Persistence/EFC/Repositories/TutorialRepository.cs
@@ -0,0 +1,19 @@
+﻿using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Infrastructure.Persistence.EFC.Repositories
+{
+    public class TutorialRepository(AppDbContext context) : BaseRepository<Tutorial>(context), ITutorialRepository
+    {
+        public async Task<IEnumerable<Tutorial>> FindByCategoryIdAsync(int cateogoryId)
+        {
+            return await context.Set<Tutorial>()
+                .Include(tutorial => tutorial.Category)
+                .Where(tutorial => tutorial.CategoryId == cateogoryId)
+                .ToListAsync();
+        }
+    }
+}
